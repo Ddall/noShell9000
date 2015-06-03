f5aedc92492baff1be5c1c72d11dd49a05fbe0d2
Allan IRDEL <a.irdel@plan-immobilier.fr>
2015-06-03 12:05:38 +0200

    fix apply script

--- environments/main/modules/ns9_base/files/ns9-puppet-apply.sh
+++ environments/main/modules/ns9_base/files/ns9-puppet-apply.sh
@@ -3,4 +3,4 @@
 cd /etc/puppet/
 git pull origin master
 librarian-puppet update
-puppet apply /etc/puppet/environments/main/manifests/site.pp --environment=main --modulepath=/etc/puppet/environments/main/modules:modules/etc/puppet/modules
+puppet apply /etc/puppet/environments/main/manifests/site.pp --environment=main --modulepath=/etc/puppet/environments/main/modules:/etc/puppet/modules

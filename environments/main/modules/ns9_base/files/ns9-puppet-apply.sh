4c250a3bff342bfdfe6d2d3660966f05c5a0503f
Allan IRDEL <a.irdel@plan-immobilier.fr>
2015-06-03 12:06:50 +0200

    shorten apply exec time

--- environments/main/modules/ns9_base/files/ns9-puppet-apply.sh
+++ environments/main/modules/ns9_base/files/ns9-puppet-apply.sh
@@ -2,5 +2,5 @@
 
 cd /etc/puppet/
 git pull origin master
-librarian-puppet update
+#librarian-puppet update
 puppet apply /etc/puppet/environments/main/manifests/site.pp --environment=main --modulepath=/etc/puppet/environments/main/modules:/etc/puppet/modules

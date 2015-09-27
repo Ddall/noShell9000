9d75598796187f94934e8f770b3cf976da06e0b7
Allan IRDEL <allan.irdel@gmail.com>
2015-09-27 14:21:13 +0200

    +key

--- environments/main/modules/ns9_users/manifests/init.pp
+++ environments/main/modules/ns9_users/manifests/init.pp
@@ -13,11 +13,18 @@ class ns9_users{
     key  => 'AAAAB3NzaC1yc2EAAAABJQAAAQEAv6WhtghYhobEa/al2zl+6gJ5IHRHJRH/l0W50Uq+07bhgom81zf8NSQ/CrJ05cxmvXBnEKhCzHdl9kKllFSLog/ijdUb6UYJxYagETBby+RNUsB0dwB/NqUK+vOoH+f8ZUYW6ZAGp4gfQcxL6AoJOu2NnHjxinwqEX1FFbQQC3uNEGHdo3M8Q6/8PYOKcc8ZW9s2xJaKfIQWCgfmpNfo4rOZQNmLRzRCKjj3TesbAhtZIrL2b89gjqsuFNzPCQuBNLvc1kWtsIcA0J2DOaWxSp/uSzu10xyhNydELTSQgdRrXabF2cx+kGk5OwtLNuag5WpKhXPr3lPjWehxJUpaFQ==',
   }
 
-  ssh_authorized_key { 'ddx-laptop-key':
+  ssh_authorized_key { 'ddx-perso-2013':
     user => 'ddall',
     type => 'ssh-rsa',
-    key  => 'AAAAB3NzaC1yc2EAAAABJQAAAQEAhmGbhNEcUzZUrqEqQEUokimprTZ4MtJ4BfHGhjG7BR72i1g6dKKbanplYngVbnS+YLk67KVxuwi3qP/s5FNLyKZ/q8POx4piScRwy+EB5heKXRFPMwHo8XXheOHcS3GtKjLrcoivzGaxGczz+fIGIQ2LIvZ/QeILhe7q535KrXd11H+iyRK3gmh+IZKQ0IPPdJbRyEELBSEVyV9K6dXelYdVKKa1/TW9PM3bWWthVq5IBqZ+paEU2WYGSXIkeIPY1zHu/RoetUitokyKIHcC42NokeW/6oUxBoMpddZKN4rxKF6xS7X/YWkiAQIMS7AZC6r9QGcQFN6CHEdOkerqxQ==',
+    key  => 'AAAAB3NzaC1yc2EAAAABJQAAAIEAoRLd705AdzuG8xr+HC9nwCIJeOanwEBL3WLnqDDZyNIeUQAaNZqu66MHvnJ6FLAPWAVmCYFUXd4w9Qb0nrVdD62iq3U2Hn01WP3/JX/LjTQXm6kczbSRbFiMqMG+6X9PeVYQteqfGm0qpd0XAFKu521iOY3XIBUCKGbWJ4aZrVk=',
   }
+
+#  ssh_authorized_key { 'ddx-perso':
+#    user => 'ddall',
+#    type => 'ssh-rsa',
+#    key  => '',
+#  }
+
 
   file_line { 'ddall ALL=(ALL) NOPASSWD: ALL':
     ensure => present,

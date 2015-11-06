d4a0db79a845e1ca2b832283e9fbb6081d9370b0
Allan IRDEL <allan.irdel@gmail.com>
2015-11-06 01:07:29 +0100

    spaces

--- environments/main/modules/ns9_smartrobot/files/smartrobot_install.sh
+++ environments/main/modules/ns9_smartrobot/files/smartrobot_install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-/usr/bin/git  clone https://github.com/Ddall/SmartRobot.git
+/usr/bin/git clone https://github.com/Ddall/SmartRobot.git
+cd SmartRobot
+/usr/local/bin/composer.phar update
